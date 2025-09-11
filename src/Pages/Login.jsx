9651219c0d467848930efc55de716e3b11b84cca
Joey Putsoedi <joeyputsoedi6@gmail.com>
2025-09-11 21:24:39 +0200

    Login-signup-form-validation
    The forms on the project had no form validation so I added some validation for both the login and sign up page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,20 +1,53 @@
 import { React, useState } from "react";
-
 import "../Styles/Login.css";
 import NavBar from "../Components/NavBar";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../Components/firebase";
 import { toast } from "react-toastify";
 import Google from "../Components/signInWithGoogle";
-/*FUNCTION----------------------------------------------- */
+
+/*FUNCTIONS---------------------------------------------------------------------- */
+
 const Login = () => {
   /*USE STATE DEFINING------------------------------- */
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({}); //State to hold error messages
 
-  /*FIREBASE AUTH------------------------------ */
+  /*FUNCTION FOR VALIDATION------------------------------------ */
+  const validateForm = () => {
+    let formErrors = {}; //local variable to hold error messages
+    let isValid = true;
+
+    if (!email) {
+      //if no email is entered
+      formErrors.email = "Email is required";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      //if email doesn't satisfy regular expression
+      formErrors.email = "Enter a valid email address";
+      isValid = false;
+    }
+
+    if (!password) {
+      //if no password is entered
+      formErrors.password = "Password is required";
+      isValid = false;
+    } else if (password.length < 6) {
+      //if password length is less than 6 characters
+      formErrors.password = "Password must be more than 6 characters";
+      isValid = false;
+    }
+
+    setErrors(formErrors); //update state with error messages
+    return isValid; //retrun whether validation is passed or not
+  };
+
+  /*FIREBASE AUTH FOR LOGIN------------------------------ */
   const handleLogin = async (e) => {
     e.preventDefault();
+
+    if (!validateForm()) return;
     try {
       await signInWithEmailAndPassword(auth, email, password);
       console.log("User login successful");
@@ -29,7 +62,6 @@ const Login = () => {
       });
     }
   };
-
   /*RETURN------------------------------ */
   return (
     <>
@@ -38,21 +70,25 @@ const Login = () => {
         <div className="loginCont">
           <h1>Login</h1>
           {/*FORM------------------------------------------ */}
+
           <form onSubmit={handleLogin}>
             {/*EMAIL INPUT---------------------------------- */}
             <label className="label" htmlFor="email">
               Email:
             </label>
             <br />
+
             <input
               type="text"
               placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              required
             />
+            {errors.email && <p className="error">{errors.email}</p>}
+            {/*if errors.email exists render <p> with error text */}
             <br />
             <br />
+
             {/*PASSWORD INPUT---------------------------------- */}
             <label className="label" htmlFor="password">
               Password:
@@ -61,12 +97,14 @@ const Login = () => {
             <input
               type="password"
               placeholder="Enter your Password"
-              required
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+            {errors.password && <p className="error">{errors.password}</p>}
+            {/*if errors.password exists render <p> with error text */}
             <br />
             <br />
+
             {/*LOGIN BUTTON-------------------------------------- */}
             <button
               style={{
