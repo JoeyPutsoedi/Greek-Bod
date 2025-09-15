28be0a2efc7bdd3b6b42a9718c91d2348b8dd530
Joey Putsoedi <joeyputsoedi6@gmail.com>
2025-09-15 15:37:20 +0200

    added-comments

--- src/Components/MealCont.jsx
+++ src/Components/MealCont.jsx
@@ -9,8 +9,26 @@ const MealCont = ({ num, type, cl, bkg }) => {
 
   //if profile exists return meals
   useEffect(() => {
-    if (profile) {
-      getMealRecommendations(profile).then(setMeals);
+    if (!profile) return;
+
+    const today = new Date().toISOString().split("T")[0]; //stores today's date
+    const storedPlan = JSON.parse(localStorage.getItem("dailyMealPlan")); //stores today's meals
+
+    //if there is a meal stored in "storedPlan" & the date it was stored matches the date stored in "today"
+    //then set meals to today's meals
+    if (storedPlan && storedPlan.Date === today) {
+      setMeals(storedPlan.meals);
+      return;
+    } else {
+      //fetch meals from the Api and store them in item "dailyMealPlan" in localStorag.
+      // store them with the date inside variable "today".
+      getMealRecommendations(profile).then((fetchedMeals) => {
+        setMeals(fetchedMeals);
+        localStorage.setItem(
+          "dailyMealPlan",
+          JSON.stringify({ date: today, meals: fetchedMeals })
+        );
+      });
     }
   }, [profile]);
 
