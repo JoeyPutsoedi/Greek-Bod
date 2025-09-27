517483a22566b949aa1a7f75703d02767e6983d2
Joey Putsoedi <joeyputsoedi6@gmail.com>
2025-09-27 14:32:14 +0200

    Update PopUp.jsx

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import { useAuth } from "../Context/AuthContext.jsx";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
-import "../styles/popup.css";
+
+import "../Styles/popup";
 
 const PopUp = () => {
   const { user } = useAuth();
